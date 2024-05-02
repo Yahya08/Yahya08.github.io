571918671e6b6d877f38e568707f6a4bb6b54094
Yahya08 <yahyahanda.r@students.amikom.ac.id>
2024-05-02 10:30:16 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Mengatur rute untuk halaman utama
+Route::get('/', [FrontController::class, 'index']);
+
+
+// Mengatur rute untuk halaman dashboard
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Mengelompokkan rute yang memerlukan autentikasi
+Route::middleware('auth')->group(function () {
+    // Rute untuk mengedit profil pengguna
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    
+    // Rute untuk memperbarui profil pengguna
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    
+    // Rute untuk menghapus profil pengguna
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Rute untuk sumber daya 'posts' yang terkait dengan PostController
+    Route::resource('posts', PostController::class);
+});
+
+// Mengimpor rute untuk otentikasi pengguna
+require __DIR__.'/auth.php';
